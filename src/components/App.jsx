0c843fe5701cd14dcd962b46123d85f069471fd5
mmoskovchuk <Maksim_Moskovchuk@govitall.com>
2017-11-23 12:37:00 +0200

    created the application component and imported it into the index.js fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,7 @@
 import React from 'react';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Header/>
-                <Content/>
-            </div>
-        );
-    }
-}
+var ui =
+    React.createElement('div', {},
+        React.createElement('h1', {}, "Hello World"),);
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        );
-    }
-}
-
-class Content extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Content</h2>
-                <p>The content text!!!</p>
-            </div>
-        );
-    }
-}
-
-export default App;
+export default ui;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import ui from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+ReactDOM.render(ui, document.getElementById('root'));
