5825b8eb392f80501aed70773ca60dde59b64c2d
mmoskovchuk <Maksim_Moskovchuk@govitall.com>
2017-11-23 11:34:39 +0200

    created the application component and imported it into the index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 ReactDOM.render(
     <h1>Hello, world!</h1>,
